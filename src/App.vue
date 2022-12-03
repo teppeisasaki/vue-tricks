71218e740b2ab4d5b3f9d3fb3140cc13f0ea995b
Teppei Sasaki <sky.contrail.man+cw@gmail.com>
2022-12-04 02:55:15 +0900

    Added initial files

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Hello, {{ val }}!</h1>
+  <EmitTestButton v-on:emitTrigger="eventHandler">Click Me!</EmitTestButton>
+</template>
+
+<script>
+  import EmitTestButton from "./components/EmitTestButton.vue";
+
+  export default {
+    components: {
+      EmitTestButton
+    },
+    data() {
+      return {
+        val: "TEST",
+      };
+    },
+    methods: {
+      eventHandler(val) {
+        this.val = val;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/EmitTestButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button v-on:click="clickHandler">Click me!</button>
+</template>
+
+<script>
+  export default {
+    methods: {
+      clickHandler() {
+        this.$emit("emitTrigger", "emitParam");
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
